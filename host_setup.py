d7d8e9dbf58a30dd16cb400e475fafc828129a91
Cloud User <gdufourcq@accelize.com>
2020-02-21 16:46:46 +0000

    First content

--- host_setup.py
+++ host_setup.py
@@ -36,6 +36,19 @@ def exec_cmd_with_ret_output(cmd, path='.'):
         raise(e)     
 
 
+def fpga_board_list():
+    fpga_boards=[]
+    ret, out, err = exec_cmd_with_ret_output('sudo lspci  -d 10ee: | grep " Processing accelerators" | grep "Xilinx" | cut -d" " -f7')
+    for line in out.splitlines():
+        if '5000' in line: fpga_boards.append('u200')
+        if '5004' in line: fpga_boards.append('u250')
+        if '500c' in line: fpga_boards.append('u280')
+        if '5020' in line: fpga_boards.append('u50')
+    
+    fpga_boards = list(dict.fromkeys(fpga_boards))
+    return fpga_boards
+
+
 def get_xrt_version(host_os):
     xrt=None
     if 'centos' in host_os.lower():
@@ -58,14 +71,6 @@ def get_host_env():
     return host_os
     
 def get_fpga_env(host_os):
-    
-    # Get XRT version
-    xrt_version = get_xrt_version(host_os).split('-')[0]
-    if not xrt_version: 
-        print(f" [ERROR] No XRT Installed. Please install Xilinx XRT package and run the script again.")
-        sys.exit(0)
-    print_status('Detected XRT', f'{xrt_version}')
-
     # Get FPGA Boards & Shells
     ret, out, err = exec_cmd_with_ret_output("/opt/xilinx/xrt/bin/xbutil list | grep Found | cut -d' ' -f4")
     nbBoardsFound = out.strip()
@@ -92,7 +97,7 @@ def get_fpga_env(host_os):
     print_status('Detected Boards', f'{boards}')
     print_status('Detected Shells', f'{shells}')
     
-    return boards, shells, xrt_version
+    return boards, shells
 
 
 def dsa_format(dsa):
@@ -102,20 +107,34 @@ def dsa_format(dsa):
     return dsa
 
 
-def check_xrt(current_ver, target_version):
-    if current_ver in target_version :
-        print_status('XRT Version Check', f'OK ({current_ver})')
-        return None
+def check_xrt(host_os, target_version):
+    if not check_host_pkg_installed(host_os, 'xrt'):
+        return True
+    
+    xrt_version = get_xrt_version(host_os).split('-')[0]
+    if not xrt_version: 
+        return True
+    print_status('Detected XRT', f'{xrt_version}')
+    
+    if xrt_version in target_version :
+        print_status('XRT Version Check', f'OK ({xrt_version})')
+        return False
     print_status('XRT Version Check', 'Failed')
-    return target_version
+    return True
 
  
-def check_dsa(current_ver, target_version):
-    if current_ver in target_version :
-        print_status('Board Shell [HOST] Version Check', f'OK ({current_ver})')
-        return None
-    print_status('Board Shell [HOST] Version Check', 'Failed')
-    return target_version
+def check_host_dsa(host_os, conf_pkg):
+    #"dsa_package":"xilinx-u200-xdma-201830.2-2580015_16.04.deb"
+    pkg_name=conf_pkg.split('-')[0]+'-'+conf_pkg.split('-')[1]+'-'+conf_pkg.split('-')[2]
+    pkg_vers=conf_pkg.split('-')[3]
+    
+    ret, out, err = exec_cmd_with_ret_output(f'sudo yum list installed | grep {pkg_name} | grep {pkg_vers} > /dev/null 2>&1')
+    
+    if ret:
+        print_status('Board Shell [HOST] Version Check', f'Failed ({pkg_name}-{pkg_vers})')
+        return True 
+    print_status('Board Shell [HOST] Version Check', f'OK ({pkg_name}-{pkg_vers})')    
+    return False
 
 
 def check_docker(host_os):
@@ -169,11 +188,11 @@ def check_host_pkg_installed(host_os, pkg):
     return True
 
 
-def host_pkg_install(host_os, packages):
+def host_pkg_install(host_os, packages):    
     if 'ubuntu' in host_os:
-        cmd = 'sudo apt-get install -y '+ packages
+        cmd = 'sudo apt-get install -y '+ packages + '> /dev/null 2>&1'
     elif 'centos' in host_os:
-        cmd = 'sudo yum install -y '+ packages
+        cmd = 'sudo yum install -y '+ packages + '> /dev/null 2>&1'
     ret, out, err = exec_cmd_with_ret_output(cmd)
     if ret:
         raise
@@ -181,9 +200,9 @@ def host_pkg_install(host_os, packages):
  
 def host_pkg_remove(host_os, packages):
     if 'ubuntu' in host_os:
-        cmd = 'sudo apt-get remove -y '+ packages
+        cmd = 'sudo apt-get remove -y '+ packages + '> /dev/null 2>&1'
     elif 'centos' in host_os:
-        cmd = 'sudo yum remove -y '+ packages
+        cmd = 'sudo yum remove -y '+ packages + '> /dev/null 2>&1'
     ret, out, err = exec_cmd_with_ret_output(cmd)
     if ret:
         raise
@@ -230,6 +249,8 @@ def host_reboot(cold=False):
         answer = input(f" > Do you want to {cmd} now (y/n)? ").lower()
     if answer == 'y':
         run('sudo '+cmd, shell=True)
+    else:
+        sys.exit(1)
 
 
 def board_shell_flash(boardIdx):
@@ -279,41 +300,52 @@ def update_host_env(host_os, update_kernel, dependencies, install_docker=False):
         sys.exit(0)
 
 
+def update_fpga_env(host_os, selected_conf, xrt_outdated, host_dsa_outdated):
+        print(f"")
+    print(f" > Packages install/update:")
+    if xrt_outdated: print(f" > \tXRT ({selected_conf['xrt_package']})")
+    if host_dsa_outdated: print(f" > \tBoard Shell [HOST] ({selected_conf['dsa_package']})")
 
-def update_fpga_env(host_os, board_idx, update_xrt=None, update_dsa=None, update_boardshell=False):
+    answer = None
+    while answer not in ['y', 'n']:
+        answer = input(f" > Start update process (y/n)? ").lower()
+    if answer == 'y':
+        if xrt_outdated:
+            print_status('Updating XRT', '')
+            host_pkg_remove(host_os, 'xrt')
+            pkg_path=host_pkg_download(selected_conf['xrt_package'])
+            host_pkg_install(host_os, pkg_path)
+            print_status('Updating XRT', 'Done')
+
+        # Check Installed versions of Host DSA against selected_conf['dsa_package']
+        if host_dsa_outdated:
+            print_status('Updating Board Shell [HOST]', '')
+            pkg_path=host_pkg_download(selected_conf['dsa_package'])
+            host_pkg_install(host_os, pkg_path)
+            print_status('Updating Board Shell [HOST]', 'Done')
+            
+        # Reboot
+        print(f" > Packages install/update completed, please reboot your server")
+        host_reboot(cold=False)
+    else:
+        sys.exit(0)  
+        
+          
+def update_board_dsa(board_idx):
     print(f"")
     print(f" > Packages install/update:")
-    if update_xrt: print(f" > \tXRT ({update_xrt})")
-    if update_dsa: print(f" > \tBoard Shell [HOST] ({update_dsa})")
-    if update_boardshell: print(f" > \tBoard Shell [FPGA])")
+    print(f" > \tBoard Shell [FPGA])")
     
     answer = None
     while answer not in ['y', 'n']:
         answer = input(f" > Start update process (y/n)? ").lower()
     if answer == 'y':
-        if update_xrt:
-            print_status('Updating XRT', '')
-            host_pkg_remove(host_os, 'xrt')
-            pkg_path=host_pkg_download(update_xrt)
-            host_pkg_install(host_os, pkg_path)
-            print_status('Updating XRT', 'Done')
-        if update_dsa:
-            print_status('Updating Board Shell [HOST]', '')
-            pkg_path=host_pkg_download(update_dsa)
-            host_pkg_install(host_os, pkg_path)
-            board_shell_flash(board_idx)
-            print_status('Updating Board Shell [HOST]', 'Done')
-        if update_boardshell:
-            print_status('Programming Board Shell [FPGA]', '')
-            board_shell_flash(board_idx)
-            print_status('Programming Board Shell [FPGA]', 'Done')
+        print_status('Programming Board Shell [FPGA]', '')
+        board_shell_flash(board_idx)
+        print_status('Programming Board Shell [FPGA]', 'Done')
         
-        if update_dsa or update_boardshell:
-            print(f" > Packages install/update completed, please do a cold reboot of your server")
-            host_reboot(cold=True)
-        else:
-            print(f" > Packages install/update completed, please reboot your server")
-            host_reboot(cold=False)
+        print(f" > Packages install/update completed, please do a cold reboot of your server")
+        host_reboot(cold=True)
     else:
         sys.exit(0)
 
@@ -345,19 +377,47 @@ def run_setup(skip, vendor, appname):
     dkr_updt=check_docker(host_os)
     if dkr_updt or krn_updt or dep_updt:
         update_host_env(host_os, krn_updt, dep_updt, dkr_updt)
-    
-    # Detect FPGA environement
-    boards, shells, xrt_version = get_fpga_env(host_os)
+        
+    # List FPGA Boards using lspci (XRT not needed)
+    lspci_boards=fpga_board_list()
     
     # Check Host Compatibility (OS, FPGA Boards)
     if not host_os in appdef['Supported']['os']:
         print(f" [ERROR] Operating System {host_os}] not supported")
         sys.exit(1)
     print_status('OS Compatibility', 'OK')
-    if not any(item in boards for item in appdef['Supported']['boards']):
-        print(f" [ERROR] Boards {boards} not supported by this application")
+    if not any(item in lspci_boards for item in appdef['Supported']['boards']):
+        print(f" [ERROR] Boards {lspci_boards} not supported by this application")
         sys.exit(1)
     print_status('FPGA Board Compatibility', 'OK')
+    
+    # Board Model Selection
+    if len(lspci_boards) > 1:
+        print(f"\n > Found {len(lspci_boards)} board models")
+        for i in range(0,len(lspci_boards)):
+            print(f"\t[{i}]: {lspci_boards[i]}")
+        board_model_idx = int(input(" > Please select the model to use: "))
+    print(f" > Selected board model: {lspci_boards[board_model_idx]}\n")
+    print_status('Selected board model', f'{lspci_boards[board_model_idx]}')
+    
+    # Find suitable configuration
+    selected_conf=None
+    for conf in appdef['HostSetupConfiguration']:
+        if host_os == conf['os'] and lspci_boards[board_model_idx] in conf['board']:
+            print_status('Suitable App Configuration', 'OK')
+            selected_conf=conf
+    if not selected_conf:
+        print_status('Suitable App Configuration', 'Failed')
+        sys.exit(1)
+        
+    # Check Installed versions of XRT against selected_conf['xrt_package']
+    xrt_outdated = check_xrt(host_os, conf['xrt_package'])
+    host_dsa_outdated = check_host_dsa(host_os, selected_conf['dsa_package'])
+    if xrt_outdated or host_dsa_outdated:
+        update_fpga_env(host_os, selected_conf, xrt_outdated, host_dsa_outdated)
+    
+    # Detect FPGA environement
+    boards, shells = get_fpga_env(host_os)
     
     # Board Selection
     board_idx=0
@@ -366,17 +426,12 @@ def run_setup(skip, vendor, appname):
         for i in range(0,len(boards)):
             print(f"\t[{i}]: {boards[i]} ({shells[i]})")
         board_idx = int(input(" > Please select the one to use: "))
-    print(f" > Selected board {board_idx}: {boards[board_idx]} {shells[board_idx]}\n")
-
-    # Find suitable configuration
-    for conf in appdef['HostSetupConfiguration']:
-        if host_os == conf['os'] and boards[board_idx] in conf['board']:
-            xrt_updt=check_xrt(xrt_version, conf['xrt_package'])
-            dsa_updt=check_dsa(shells[board_idx], dsa_format(conf['dsa_package']))
-            brd_updt=check_board_shell(board_idx)
-            
-            if xrt_updt or dsa_updt or brd_updt:
-                update_fpga_env(host_os, board_idx, xrt_updt, dsa_updt, brd_updt)
+    print_status('Selected board', f'{board_idx}: {boards[board_idx]} {shells[board_idx]}')
+    
+    # Check Board DSA
+    board_dsa_outdated=check_board_shell(board_idx)
+    if board_dsa_outdated:
+        update_board_dsa(board_idx)
                 
     # Check Access Key
     homedir = os.environ['HOME']
