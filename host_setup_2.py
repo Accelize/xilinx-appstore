fa0299cf58fda6c0fb8f8d71cbb409dbae5fcdfa
Cloud User <gdufourcq@accelize.com>
2020-04-03 21:04:58 +0000

    Remove automated board programmation

--- host_setup_2.py
+++ host_setup_2.py
@@ -244,7 +244,7 @@ def check_board_shell(boardIdx):
     print_status('Board Shell Host vs. FPGA', 'Update Required')
     cmd=get_board_shell_flash_cmd(boardIdx)
     print(' > Update Board DSA using the following command, then do a cold reboot:')
-    print(cmd)
+    print(' > %s' % cmd)
     sys.exit(1)
 
 
